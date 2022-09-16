bf32fb5e46bec731c6e0364bf1d82e70711612a6
PradeepKumar <83265168+pradeep14598@users.noreply.github.com>
2022-09-16 10:55:32 +0530

    Update get_state.dart

--- lib/get_state_manager/src/simple/get_state.dart
+++ lib/get_state_manager/src/simple/get_state.dart
@@ -30,8 +30,7 @@ extension ReadExt on BuildContext {
 //     return Bind.of(this, filter: filter, rebuild: true);
 //   }
 // }
-
-class GetBuilder<T extends GetxController> extends StatelessWidget {
+class GetBuilder<T extends GetxController> extends StatefulWidget {
   final GetControllerBuilder<T> builder;
   final bool global;
   final Object? id;
@@ -39,10 +38,10 @@ class GetBuilder<T extends GetxController> extends StatelessWidget {
   final bool autoRemove;
   final bool assignId;
   final Object Function(T value)? filter;
-  final void Function(BindElement<T> state)? initState,
+  final void Function(GetBuilderState<T> state)? initState,
       dispose,
       didChangeDependencies;
-  final void Function(Binder<T> oldWidget, BindElement<T> state)?
+  final void Function(GetBuilder oldWidget, GetBuilderState<T> state)?
       didUpdateWidget;
   final T? init;
 
@@ -62,29 +61,190 @@ class GetBuilder<T extends GetxController> extends StatelessWidget {
     this.didUpdateWidget,
   }) : super(key: key);
 
+  // static T of<T extends GetxController>(
+  //   BuildContext context, {
+  //   bool rebuild = false,
+  // }) {
+  //   var widget = rebuild
+  //       ? context
+  //       .dependOnInheritedWidgetOfExactType<_InheritedGetxController<T>>()
+  //       : context
+  //           .getElementForInheritedWidgetOfExactType<
+  //               _InheritedGetxController<T>>()
+  //           ?.widget;
+
+  //   if (widget == null) {
+  //     throw 'Error: Could not find the correct dependency.';
+  //   } else {
+  //     return (widget as _InheritedGetxController<T>).model;
+  //   }
+  // }
+
+  @override
+  GetBuilderState<T> createState() => GetBuilderState<T>();
+}
+
+class GetBuilderState<T extends GetxController> extends State<GetBuilder<T>>
+    with GetStateUpdaterMixin {
+  T? controller;
+  bool? _isCreator = false;
+  VoidCallback? _remove;
+  Object? _filter;
+
+  @override
+  void initState() {
+    // _GetBuilderState._currentState = this;
+    super.initState();
+    widget.initState?.call(this);
+
+    var isRegistered = GetInstance().isRegistered<T>(tag: widget.tag);
+
+    if (widget.global) {
+      if (isRegistered) {
+        if (GetInstance().isPrepared<T>(tag: widget.tag)) {
+          _isCreator = true;
+        } else {
+          _isCreator = false;
+        }
+        controller = GetInstance().find<T>(tag: widget.tag);
+      } else {
+        controller = widget.init;
+        _isCreator = true;
+        GetInstance().put<T>(controller!, tag: widget.tag);
+      }
+    } else {
+      controller = widget.init;
+      _isCreator = true;
+      controller?.onStart();
+    }
+
+    if (widget.filter != null) {
+      _filter = widget.filter!(controller!);
+    }
+
+    _subscribeToController();
+  }
+
+  /// Register to listen Controller's events.
+  /// It gets a reference to the remove() callback, to delete the
+  /// setState "link" from the Controller.
+  void _subscribeToController() {
+    _remove?.call();
+    _remove = (widget.id == null)
+        ? controller?.addListener(
+            _filter != null ? _filterUpdate : getUpdate,
+          )
+        : controller?.addListenerId(
+            widget.id,
+            _filter != null ? _filterUpdate : getUpdate,
+          );
+  }
+
+  void _filterUpdate() {
+    var newFilter = widget.filter!(controller!);
+    if (newFilter != _filter) {
+      _filter = newFilter;
+      getUpdate();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    widget.dispose?.call(this);
+    if (_isCreator! || widget.assignId) {
+      if (widget.autoRemove && GetInstance().isRegistered<T>(tag: widget.tag)) {
+        GetInstance().delete<T>(tag: widget.tag);
+      }
+    }
+
+    _remove?.call();
+
+    controller = null;
+    _isCreator = null;
+    _remove = null;
+    _filter = null;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    widget.didChangeDependencies?.call(this);
+  }
+
+  @override
+  void didUpdateWidget(GetBuilder oldWidget) {
+    super.didUpdateWidget(oldWidget as GetBuilder<T>);
+    // to avoid conflicts when modifying a "grouped" id list.
+    if (oldWidget.id != widget.id) {
+      _subscribeToController();
+    }
+    widget.didUpdateWidget?.call(oldWidget, this);
+  }
+
   @override
   Widget build(BuildContext context) {
-//     return Binder(
-//       init: init == null ? null : () => init!,
-//       global: global,
-//       autoRemove: autoRemove,
-//       assignId: assignId,
-//       initState: initState,
-//       filter: filter,
-//       tag: tag,
-//       dispose: dispose,
-//       id: id,
-//       lazy: false,
-//       didChangeDependencies: didChangeDependencies,
-//       didUpdateWidget: didUpdateWidget,
-//       child: Builder(builder: (context) {
-//         final controller = Bind.of<T>(context, rebuild: true);
-//         return builder(controller);
-//       }),
-//     );
-  return builder(controller!);
+    // return _InheritedGetxController<T>(
+    //   model: controller,
+    //   child: widget.builder(controller),
+    // );
+    return widget.builder(controller!);
   }
 }
+// class GetBuilder<T extends GetxController> extends StatelessWidget {
+//   final GetControllerBuilder<T> builder;
+//   final bool global;
+//   final Object? id;
+//   final String? tag;
+//   final bool autoRemove;
+//   final bool assignId;
+//   final Object Function(T value)? filter;
+//   final void Function(BindElement<T> state)? initState,
+//       dispose,
+//       didChangeDependencies;
+//   final void Function(Binder<T> oldWidget, BindElement<T> state)?
+//       didUpdateWidget;
+//   final T? init;
+
+//   const GetBuilder({
+//     Key? key,
+//     this.init,
+//     this.global = true,
+//     required this.builder,
+//     this.autoRemove = true,
+//     this.assignId = false,
+//     this.initState,
+//     this.filter,
+//     this.tag,
+//     this.dispose,
+//     this.id,
+//     this.didChangeDependencies,
+//     this.didUpdateWidget,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+// //     return Binder(
+// //       init: init == null ? null : () => init!,
+// //       global: global,
+// //       autoRemove: autoRemove,
+// //       assignId: assignId,
+// //       initState: initState,
+// //       filter: filter,
+// //       tag: tag,
+// //       dispose: dispose,
+// //       id: id,
+// //       lazy: false,
+// //       didChangeDependencies: didChangeDependencies,
+// //       didUpdateWidget: didUpdateWidget,
+// //       child: Builder(builder: (context) {
+// //         final controller = Bind.of<T>(context, rebuild: true);
+// //         return builder(controller);
+// //       }),
+// //     );
+//   return builder(controller!);
+//   }
+// }
 
 abstract class Bind<T> extends StatelessWidget {
   const Bind({
